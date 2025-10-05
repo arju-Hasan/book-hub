d1d21d22bd2e0eec70996039b7af379bff27a30b
Arju-Hasan <rajuahmeed698@gmail.com>
2025-10-05 14:53:34 +0600

    banner trxt update

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
       <div className="flex flex-col md:flex-row justify-around items-center bg-[#F3F3F3] m-2 gap-4 p-4 rounded-2xl">
         <div>
           <h2 className="text-4xl text-black">
-            Books to freshen up <br /> your bookshelf
+            Arju to freshen up <br /> your bookshelf
           </h2>
           <button className="btn rounded-md m-4 font-bold text-white bg-green-600">
             View The List
